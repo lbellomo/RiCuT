1b0900bbc4714699d846ba056e46675a4d159466
pbovina <pbovina@ascentio.com.ar>
2017-04-30 16:07:09 -0300

    implementacion funcion respuesta

--- bot/services.py
+++ bot/services.py
@@ -3,8 +3,11 @@ import json
 from math import sin, cos, sqrt, atan2, radians
 from key import keys
 from urllib.request import urlopen
-#from make_map import make_map
-#from ellipse import function
+
+"""
+from make_map import make_map
+from ellipse import ellipse
+"""
 
 def get_wind_data(lat=35, lon=139):
     app_id = keys['owm']
@@ -12,7 +15,7 @@ def get_wind_data(lat=35, lon=139):
     url = url.format(lat,lon, app_id) 
     r = requests.get(url)
     j = json.loads(r.text)
-    return j.get('wind')['deg'],j.get['wind']['speed']
+    return j.get['wind']['speed'],j.get('wind')['deg']
 
 
 def get_modis():
@@ -28,8 +31,8 @@ def get_modis():
     pares = [float((e.split(',')[0]), float(e.split(',')[1])) for e in d] #armamos pares
     return [pares[0]]
 
-def get_image(path="file:///home/pbovina/space/RiCuT/bot/maps/map.png", name='map.png'):
-    photo = urlopen(path)
+def get_image(path="file:///home/pbovina/space/RiCuT/bot/maps/{}", name='map.png'):
+    photo = urlopen(path.format(name))
     return name, photo
 
 def get_message(peligro=True, lat1=10.22, lon1=10.22, lat2=15.22, lon2=15.22, direc="norte"):
@@ -49,17 +52,27 @@ def get_message_distance(peligro=True, lat1=10.22, lon1=10.22, lat2=15.22, lon2=
     msg1 = "Usted esta a salvo respecto a la zona de peligro a una distancia de {}"
     return msg1.format(dist)
 
-def respuesta(pp_lat1,pp_lon1):
+"""
+def respuesta(pp_lat,pp_lon):
     fuego = get_modis()[0] # par de lat lon del fuego
     viento = get_wind_data(fuego[0], fuego[1]) # obtenemos viendo en centro del fuego
-    #function(fi_lat, fi_long,pp_lat, pp_long,vel_lat, vel_long,coef_A=0.5):
-    #elipse = function(fuego[0],fuego[1], pp_lat, pp_lon, viento[0],viento[1])
-    #e_cx = elipse[0](ce_y, ce_x, ps_y, ps_x, a, b, alfa)
-    #e_cy = elipse[1]
+    #return (ce_lat, ce_long, ps_lat, ps_long, a, b, alfa)
+    el = ellipse(fuego[0], fuego[1], pp_lat, pp_lon, viento[0], viento[1])
+    #(lon_e=-32.08, lat_e=-64.53, width_e=0.01, height_e=0.02,angle_e=30, lon_p=-32.085, lat_p=-64.53, lon_s=-32.083, lat_s=-64.532):    
+    name = make_map(el[0], el[1], el[4], el[5], el[6], el[2], el[3], pp_lon, pp_lat, ):   
+    
+    if el[2] == None or el[3] == None:        
+        orien = direccion_flecha(pp_lat,pp_lon,el[2],el[3]))
+        msg = get_message(False,pp_lat,pp_lon,fuego[0],fuego[1], orien)
+        photo = get_photo(name)
+        return msg, photo
 
-    #mapa = make_map()
-
-
+    orien = direccion_flecha(pp_lat,pp_lon,el[2],el[3]))
+    msg = get_message(False,pp_lat,pp_lon,fuego[0],fuego[1], orien)
+    photo = get_photo(name)
+    return msg, photo  
+"""
+    
 def dist_fuego_persona(lat1, lon1, lat2, lon2):
 
     # approximate radius of earth in km
@@ -80,4 +93,5 @@ def dist_fuego_persona(lat1, lon1, lat2, lon2):
 
     return distance
 
-
+def direccion_flecha(lat1=1, lon=1, lat2=1, lat2=1):
+    return "norte"
